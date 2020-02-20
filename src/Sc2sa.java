7509593755d2a15b91f74944ed5fe336c3c75a0b
ismailAbdelwahab <is.abdelwahab@gmail.com>
2020-02-21 00:26:06 +0100

    Continuation of TP2. Todo : check output with all the "s.out" that were added

--- src/Sc2sa.java
+++ src/Sc2sa.java
@@ -2,276 +2,323 @@ import sc.analysis.DepthFirstAdapter;
 import sc.node.*;
 import sa.*;
 
+//test with:
+// javac Compiler.java && java Compiler ../test/input/add1.l
+
 public class Sc2sa extends DepthFirstAdapter {
 	private SaNode returnValue;
 
-	public SaNode getRoot() { return returnValue; }
+	public SaNode getRoot() { return this.returnValue; }
 
 	@Override
-	public void caseStart(Start node) {
-		super.caseStart(node);
-	}
+	public void caseStart(Start node) { super.caseStart(node); }
 
-	public void caseAAffectI(AAffectI node) {
-		SaVar id = null;
-		SaExp value = null;
-		node.getVar().apply(this);
-		id = (SaVar) this.returnValue;
-		node.getE().apply(this);
-		value = (SaExp) this.returnValue;
-		this.returnValue = new SaInstAffect(id, value);
+    public void caseAAffectI(AAffectI node) {
+        node.getVar().apply(this);
+        SaVar id = (SaVar) this.returnValue;
+        node.getE().apply(this);
+        SaExp value = (SaExp) this.returnValue;
+        this.returnValue = new SaInstAffect(id, value);
 	}
 	public void caseAArrayElement(AArrayElement node) {
-		SaVarSimple nom = null;
-		SaExp indice = null;
-		node.getId().apply(this);
-		nom = (SaVarSimple) this.returnValue;
-		node.getE().apply(this);
-		indice = (SaExp) this.returnValue;
-		this.returnValue = new SaVarIndicee(nom.getNom(), indice);
+        String id = node.getId().getText();
+        node.getE().apply(this);
+        SaExp value = (SaExp) this.returnValue;
+        this.returnValue = new SaVarIndicee(id, value);
 	}
-//AArrayVar//
+    //AArrayVar//
+	public void caseAArrayVar(AArrayVar node) {
+        String id = node.getId().getText();
+		node.getNb().apply(this);
+        SaExp indice = (SaExp) this.returnValue;
+		this.returnValue = new SaVarIndicee(id, indice);
+	}
 	public void caseADivArithHighE(ADivArithHighE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
-		node.getArithHighE().apply(this);
-		op1 = (SaExp) this.returnValue;
-		node.getNegE().apply(this);
-		op2 = (SaExp) this.returnValue;
-		this.returnValue = new SaExpDiv(op1, op2);
+        node.getArithHighE().apply(this);
+        SaExp op1 = (SaExp) this.returnValue;
+        node.getNegE().apply(this);
+        SaExp op2 = (SaExp) this.returnValue;
+        this.returnValue = new SaExpDiv(op1, op2);
+		System.out.println(" -> division");
 	}
 
 	//AEpsEBis//
+	public void caseAEpsEBis(AEpsEBis node) {
+	    this.returnValue = null;
+    }
 	//AEpsElseBlock//
+	public void caseAEpsElseBlock(AEpsElseBlock node) {
+	    this.returnValue = null;
+    }
 	//AEpsIBis//
+	public void caseAEpsIBis(AEpsIBis node) {
+	    this.returnValue = null;
+    }
 	//AEpsLdf//
+	public void caseAEpsLdf(AEpsLdf node) {
+	    this.returnValue = null;
+    }
 	//AEpsLdv//
+	public void caseAEpsLdv(AEpsLdv node) {
+	    this.returnValue = null;
+    }
 	//AEpsLdvBis//
+	public void caseAEpsLdvBis(AEpsLdvBis node) {
+	    this.returnValue = null;
+    }
 	//AEpsLdvOptional//
+	public void caseAEpsLdvOptional(AEpsLdvOptional node) {
+	    this.returnValue = null;
+    }
 	//AEpsLe//
+	public void caseAEpsLe(AEpsLe node) {
+	    this.returnValue = null;
+    }
 	public void caseAEqualCompE(AEqualCompE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
-		node.getCompE().apply(this);
-		op1 = (SaExp) this.returnValue;
-		node.getArithLowE().apply(this);
-		op2 = (SaExp) this.returnValue;
-		this.returnValue = new SaExpEqual(op1, op2);
+        node.getCompE().apply(this);
+        SaExp op1 = (SaExp) this.returnValue;
+        node.getArithLowE().apply(this);
+        SaExp op2 = (SaExp) this.returnValue;
+        this.returnValue = new SaExpEqual(op1, op2);
+		System.out.println(" -> equal");
 	}
-	//AFuncCallElement//
+	public void caseAFuncCallElement(AFuncCallElement node) {
+        node.getAppFunction().apply(this);
+        SaAppel val = (SaAppel) this.returnValue;
+        this.returnValue = new SaExpAppel(val);
+	}
 
 	public void caseAFuncCallI(AFuncCallI node) {
-		SaVarIndicee op1 = null;
-		SaLExp op2 = null;
-		node.getId().apply(this);
-		op1 = (SaVarIndicee) this.returnValue;
-		node.getLe().apply(this);
-		op2 = (SaLExp) this.returnValue;
-		this.returnValue = new SaAppel(op1.getNom(), op2);
+        String id = node.getId().getText();
+        node.getLe().apply(this);
+        SaLExp params = (SaLExp) this.returnValue;
+        this.returnValue = new SaAppel(id, params);
 	}
+
 	public void caseAIfI(AIfI node) {
-		SaExp test = null;
-		SaInst alors = null;
-		SaInst sinon = null;
-		node.getE().apply(this);
-		test = (SaExp) this.returnValue;
-		node.getBi().apply(this);
-		alors = (SaInst) this.returnValue;
-		node.getElseBlock().apply(this);
-		sinon = (SaInst) this.returnValue;
-		this.returnValue = new SaInstSi( test, alors, sinon);
+        node.getE().apply(this);
+        SaExp test = (SaExp) this.returnValue;
+        node.getBi().apply(this);
+        SaInst alors = (SaInst) this.returnValue;
+        node.getElseBlock().apply(this);
+        SaInst sinon = (SaInst) this.returnValue;
+        this.returnValue = new SaInstSi(test, alors, sinon);
 	}
 	public void caseALowerThanCompE(ALowerThanCompE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
-		node.getCompE().apply(this);
-		op1 = (SaExp) this.returnValue;
-		node.getArithLowE().apply(this);
-		op2 = (SaExp) this.returnValue;
-		this.returnValue = new SaExpInf(op1, op2);
+        node.getCompE().apply(this);
+        SaExp op1 = (SaExp) this.returnValue;
+        node.getArithLowE().apply(this);
+        SaExp op2 = (SaExp) this.returnValue;
+        this.returnValue = new SaExpInf(op1, op2);
+		System.out.println(" ->  < (lower)");
 	}
 	public void caseAMoinsArithLowE(AMoinsArithLowE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
-		node.getArithLowE().apply(this);
-		op1 = (SaExp) this.returnValue;
-		node.getArithHighE().apply(this);
-		op2 = (SaExp) this.returnValue;
-		this.returnValue = new SaExpSub(op1, op2);
+        node.getArithLowE().apply(this);
+        SaExp op1 = (SaExp) this.returnValue;
+        node.getArithHighE().apply(this);
+        SaExp op2 = (SaExp) this.returnValue;
+        this.returnValue = new SaExpSub(op1, op2);
+		System.out.println(" -> soustraction");
 	}
 	public void caseAMultArithHighE(AMultArithHighE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
-		node.getArithHighE().apply(this);
-		op1 = (SaExp) this.returnValue;
-		node.getNegE().apply(this);
-		op2 = (SaExp) this.returnValue;
-		this.returnValue = new SaExpMult(op1, op2);
+        node.getArithHighE().apply(this);
+        SaExp op1 = (SaExp) this.returnValue;
+        node.getNegE().apply(this);
+        SaExp op2 = (SaExp) this.returnValue;
+        this.returnValue = new SaExpMult(op1, op2);
+		System.out.println(" -> multiplication");
 	}
-	//ANumberElement//
+	public void caseANumberElement(ANumberElement node) {
+		//node.getNb().apply(this);
+        //SaExpInt nb = (SaExpInt) this.returnValue;
+        int value = Integer.parseInt(node.getNb().getText());
+		this.returnValue = new SaExpInt( value );
+		System.out.println("ALAIDE Valeur trouvée");
+	}
 	/* TOUT les "APASS" sont ici */
+	public void caseAPassAndE(APassAndE node) {
+	    this.returnValue = null;
+		System.out.println(" -> passed and");
+    }
+	public void caseAPassArithHighE(APassArithHighE node) {
+	    this.returnValue = null;
+		System.out.println(" -> passed arith high");
+    }
+	public void caseAPassArithLowE(APassArithLowE node) {
+	    this.returnValue = null;
+		System.out.println(" -> passed arith low");
+    }
+	public void caseAPassCompE(APassCompE node) {
+	    this.returnValue = null;
+		System.out.println(" -> passed comparaison");
+    }
+	public void caseAPassE(APassE node) {
+	    this.returnValue = null;
+		System.out.println(" -> passed or");
+    }
+	public void caseAPassNegE(APassNegE node) {
+	    this.returnValue = null;
+		System.out.println(" -> passed negation");
+    }
+	public void caseAPassParenthE(APassParenthE node) {
+	    this.returnValue = null;
+		System.out.println(" -> passed parentheses");
+    }
 
 	public void caseAPlusArithLowE(APlusArithLowE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
 		node.getArithLowE().apply(this);
-		op1 = (SaExp) this.returnValue;
+        SaExp op1 = (SaExp) this.returnValue;
 		node.getArithHighE().apply(this);
-		op2 = (SaExp) this.returnValue;
+        SaExp op2= (SaExp) this.returnValue;
 		this.returnValue = new SaExpAdd(op1, op2);
+		System.out.println(" -> addition");
 	}
+
 	public void caseAProgProgramme(AProgProgramme node) {
-		SaLDec ldvo = null;
-		SaLDec ldf = null;
 		node.getLdvOptional().apply(this);
-		ldvo = (SaLDec) this.returnValue;
+        SaLDec ldvo = (SaLDec) this.returnValue;
 		node.getLdf().apply(this);
-		ldf = (SaLDec) this.returnValue;
+        SaLDec ldf = (SaLDec) this.returnValue;
 		this.returnValue = new SaProg(ldvo, ldf);
 	}
 	public void caseAReadElement(AReadElement node) {
 		this.returnValue = new SaExpLire();
 	}
 	public void caseARegularAndE(ARegularAndE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
 		node.getAndE().apply(this);
-		op1 = (SaExp) this.returnValue;
+        SaExp op1 = (SaExp) this.returnValue;
 		node.getCompE().apply(this);
-		op2 = (SaExp) this.returnValue;
+        SaExp op2 = (SaExp) this.returnValue;
 		this.returnValue = new SaExpAnd(op1, op2);
+		System.out.println(" -> AND");
 	}
 	public void caseARegularAppFunction(ARegularAppFunction node) {
-		SaVarSimple nom = null;
-		SaLExp le = null;
-		node.getId().apply(this);
-		nom = (SaVarSimple) this.returnValue;
+		String id = node.getId().getText();
 		node.getLe().apply(this);
-		le = (SaLExp) this.returnValue;
-		SaAppel appel = new SaAppel(nom.getNom(), le);
+        SaLExp le = (SaLExp) this.returnValue;
+		SaAppel appel = new SaAppel(id, le);
 		this.returnValue = new SaExpAppel(appel);
 	}
 
 	public void caseARegularArrayDv(ARegularArrayDv node) {
-		SaVarIndicee nom = null;
-		SaExpInt size = null;
-		node.getId().apply(this);
-		nom = (SaVarIndicee) this.returnValue;
-		node.getNb().apply(this);
-		size = (SaExpInt) this.returnValue;
-		this.returnValue = new SaDecTab(nom.getNom(), size.getVal() );
+        String id = node.getId().getText();
+        int size = Integer.parseInt( node.getNb().getText() );
+		this.returnValue = new SaDecTab(id, size );
 	}
 	public void caseARegularBi(ARegularBi node) {
-		SaLInst li = null;
 		node.getLi().apply(this);
-		li = (SaLInst) this.returnValue;
+        SaLInst li = (SaLInst) this.returnValue;
 		this.returnValue = new SaInstBloc(li);
 	}
 	public void caseARegularDf(ARegularDf node) {
-		SaVarSimple id = null;
-		SaLDec params = null;
-		SaLDec vars = null;
-		SaInst corps = null;
-		node.getId().apply(this);
-		id = (SaVarSimple) this.returnValue;
+        String id = node.getId().getText();
 		node.getLdv().apply(this);
-		params = (SaLDec) this.returnValue;
+        SaLDec params = (SaLDec) this.returnValue;
 		node.getLdvOptional().apply(this);
-		vars = (SaLDec) this.returnValue;
+        SaLDec vars = (SaLDec) this.returnValue;
 		node.getBi().apply(this);
-		corps = (SaInst) this.returnValue;
-		this.returnValue = new SaDecFonc(id.getNom(), params, vars, corps);
+        SaInst iBloc = (SaInst) this.returnValue;
+		this.returnValue = new SaDecFonc(id, params, vars, iBloc);
 	}
 	public void caseARegularE(ARegularE node) {
-		SaExp op1 = null;
-		SaExp op2 = null;
 		node.getE().apply(this);
-		op1 = (SaExp) this.returnValue;
+        SaExp op1 = (SaExp) this.returnValue;
 		node.getAndE().apply(this);
-		op2 = (SaExp) this.returnValue;
+        SaExp op2 = (SaExp) this.returnValue;
 		this.returnValue = new SaExpOr(op1, op2);
+		System.out.println(" -> Or");
 	}
 	//ARegularEBis//
+	public void caseARegularEBis(ARegularEBis node) {
+	    this.returnValue = null;
+    }
 	//ARegularElseBlock//
+	public void caseARegularElseBlock(ARegularElseBlock node) {
+	    this.returnValue = null;
+    }
 	//ARegularIBis//
+	public void caseARegularIBis(ARegularIBis node) {
+	    this.returnValue = null;
+    }
 	public void caseARegularLdf(ARegularLdf node) {
-		SaDec df = null;
-		SaLDec ldf = null;
 		node.getDf().apply(this);
-		df = (SaDec) this.returnValue;
+        SaDec df = (SaDec) this.returnValue;
 		node.getLdf().apply(this);
-		ldf = (SaLDec) this.returnValue;
+        SaLDec ldf = (SaLDec) this.returnValue;
 		this.returnValue = new SaLDec(df, ldf);
 	}
 	public void caseARegularLdv(ARegularLdv node) {
-		SaDec dv = null;
-		SaLDec ldv = null;
 		node.getDv().apply(this);
-		dv = (SaDec) this.returnValue;
+        SaDec dv = (SaDec) this.returnValue;
 		node.getLdvBis().apply(this);
-		ldv = (SaLDec) this.returnValue;
+        SaLDec ldv = (SaLDec) this.returnValue;
 		this.returnValue = new SaLDec(dv, ldv);
 	}
 	//ARegularLdvBis//
+	public void caseARegularLdvBis(ARegularLdvBis node) {
+	    this.returnValue = null;
+    }
 	//ARegularLdvOptional//
+	public void ARegularLdvOptional(ARegularLdvOptional node) {
+	    node.getLdv().apply(this); //TODO : check this method
+    }
 	public void caseARegularLe(ARegularLe node) {
-		SaDec e = null;
-		SaLDec e_bis = null;
 		node.getE().apply(this);
-		e = (SaDec) this.returnValue;
+        SaExp e = (SaExp) this.returnValue;
 		node.getEBis().apply(this);
-		e_bis = (SaLDec) this.returnValue;
-		this.returnValue = new SaLDec(e, e_bis);
+        SaLExp e_bis = (SaLExp) this.returnValue;
+		this.returnValue = new SaLExp(e, e_bis);
 	}
 	public void caseARegularLi(ARegularLi node) {
-		SaInst head = null;
-		SaLInst tail = null;
 		node.getI().apply(this);
-		head = (SaInst) this.returnValue;
+        SaInst head = (SaInst) this.returnValue;
 		node.getIBis().apply(this);
-		tail = (SaLInst) this.returnValue;
+        SaLInst tail = (SaLInst) this.returnValue;
 		this.returnValue = new SaLInst(head, tail);
 	}
 	public void caseARegularNegE(ARegularNegE node) {
-		SaExp op1 = null;
 		node.getParenthE().apply(this);
-		op1 = (SaExp) this.returnValue;
+        SaExp op1 = (SaExp) this.returnValue;
 		this.returnValue = new SaExpNot(op1);
+		System.out.println(" -> negation");
 	}
 	//ARegularParenthE//
+	public void caseARegularParenthE(ARegularParenthE node) {
+	    node.getE().apply( this );
+		System.out.println(" -> parentheses");
+    }
 	//ARegularVar//
+	public void caseARegularVar(ARegularVar node) {
+	    String id = node.getId().getText();
+		this.returnValue = new SaVarSimple(id);
+	    //this.returnValue = null;
+    }
 	public void caseARegularVariableDv(ARegularVariableDv node) {
-		SaVarSimple nom = null;
-		node.getId().apply(this);
-		nom = (SaVarSimple) this.returnValue;
-		this.returnValue = new SaDecVar(nom.getNom());
+		String id = node.getId().getText();
+		this.returnValue = new SaDecVar( id );
 	}
 	public void caseAReturnI(AReturnI node) {
-		SaExp val = null;
 		node.getE().apply(this);
-		val = (SaExp) this.returnValue;
+        SaExp val = (SaExp) this.returnValue;
 		this.returnValue = new SaInstRetour(val);
 	}
 	public void caseAVariableElement(AVariableElement node) {
-		SaVarSimple var = null;
 		node.getId().apply(this);
-		var = (SaVarSimple) this.returnValue;
-		this.returnValue = new SaVarSimple(var.getNom());
+        SaVarSimple val = (SaVarSimple) this.returnValue;
+		this.returnValue = new SaExpVar(val);
 	}
 	public void caseAWhileI(AWhileI node) {
-		SaExp test = null;
-		SaInst bi = null;
 		node.getE().apply(this);
-		test = (SaExp) this.returnValue;
+        SaExp test = (SaExp) this.returnValue;
 		node.getBi().apply(this);
-		bi = (SaInst) this.returnValue;
+        SaInst bi = (SaInst) this.returnValue;
 		this.returnValue = new SaInstTantQue(test, bi);
 	}
 	public void caseAWriteI(AWriteI node) {
-		SaExp arg = null;
 		node.getE().apply(this);
-		arg = (SaExp) this.returnValue;
+        SaExp arg = (SaExp) this.returnValue;
 		this.returnValue = new SaInstEcriture(arg);
+		System.out.println("ALAIDE ecriture");
 	}
 
 
